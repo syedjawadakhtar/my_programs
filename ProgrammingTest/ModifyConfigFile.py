9f720e9c65834a140c8d33f43cf0c1a621d8992c
Syed Jawad Akhtar <syedjawadakhtar@gmail.com>
2022-07-08 22:52:58 +0300

    Update adding till item.. parse value and SocketIp using strings

--- ProgrammingTest/ModifyConfigFile.py
+++ ProgrammingTest/ModifyConfigFile.py
@@ -4,26 +4,46 @@ from cmath import nan
 from configparser import ConfigParser
 from fileinput import filename
 import sys
+import ast
 
 if __name__ == '__main__':
     config_object = ConfigParser(allow_no_value=True)
+    config_object.sections()
 
+    # python ModifyConfigFile.py filname [sensor_index] newIP
     filename = sys.argv[1]
+    sensor_index = sys.argv[2]
+    newIP = sys.argv[3]
+    
     #Read OriginalControls.ini
     config_object.read(filename)
-    print ("The config file name is ", filename)
 
-    sensor_index = sys.argv[2]
 
-    newIP = sys.argv[3]
-
-    if sensor_index == nan:
-        for i in range (10+1):
-            [i].SocketIP = newIP
-    else:
-        [sensor_index].SocketIP = newIP
+    # if sensor_index == nan:
+    #     for i in range (10+1):
+    #         [i].SocketIP = newIP
+    # else:
+    #     [sensor_index].SocketIP = newIP
 
     #We only need sensor settings
-    # config_object["SensorSettingsWithNames"] = {
-    #     SocketIp = 127.0.0.1
-    # }
+    if config_object['EgoVehicleInfo']['SensorSettingsWithNames']:
+        string = config_object.get('EgoVehicleInfo', 'SensorSettingsWithNames').strip().split('\n')
+        print(string.find("SocketIp"))
+        # list_in_list = ast.literal_eval(config_object['EgoVehicleInfo']['SensorSettingsWithNames'])
+        # print(list_in_list)
+
+    # for section in config_object.sections():
+    #     print(f"[{section}]")
+    #     if section == "EgoVehicleInfo":
+    #         for key, value in config_object.items(section):
+    #             if key == "sensorsettingswithnames":
+    #                 print(f"\tfor key {key} -> {value} (value)")
+    #                 if value == "[0]":
+    #                     print ("found it")
+    
+#Update the password
+# userinfo["password"] = "newpassword"
+
+# #Write changes back to file
+# with open('config.ini', 'w') as conf:
+#     config_object.write(conf)
