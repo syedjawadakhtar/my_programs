dbbbeb00918131957758937ff7766dd7e12fc69d
Syed Jawad Akhtar <syedjawadakhtar@gmail.com>
2023-04-28 23:25:42 +0300

    Solved test: Using regular expressions

--- ProgrammingTest/ModifyConfigFile.py
+++ ProgrammingTest/ModifyConfigFile.py
@@ -1,57 +1,86 @@
 #!/bin/python
 
-from cmath import nan
 from configparser import ConfigParser
-from fileinput import filename
-import sys
-import ast
+from fileinput import filename  # File handling
+import sys                      # for cmd line arguments
+import re                       # for regular expressions
 
 if __name__ == '__main__':
     config = ConfigParser(allow_no_value=True)
-    config.sections()
 
     # cmdline >>python ModifyConfigFile.py filname [sensor_index] newIP
-    filename = sys.argv[1]
-    sensor_index = sys.argv[2]
-    newIP = sys.argv[3]
-    
-    #Read OriginalControls.ini
-    config.read(filename)
+    if len(sys.argv) == 3:
+        filename = sys.argv[1]
+        newIP = str(sys.argv[2])
+         
+        #Read the configutation file
+        config.read(filename)
+        
+        for sensor_index in range (11):
+            input_str=config['EgoVehicleInfo']['SensorSettingsWithNames']
+            input_str = re.sub(r'\s+', '', input_str)
+        
+            pattern = r"\[%d\]={([^}]*).*?SocketIp=([\d\.]+)" % sensor_index
+            match = re.search(pattern, input_str)
 
-    fullIp = "SockerIp"+newIP
+            # If there is a match, extract the SocketIp value and replace it
+            if match:
+                socket_ip = match.group(2)
+                print(f"Socket IP for sensor {sensor_index}: {socket_ip}")
+                new_settings = input_str.replace(socket_ip, newIP)
+                
+                # Adding spaces in the string to look good
+                input_str = re.sub(r'\s+', '', input_str)
+                input_str = re.sub(r'([^\{\}\[\]\(\)=,]+)(=)', r'\1 = ', input_str)
 
-    # if sensor_index == nan:
-    #     for i in range (10+1):
-    #         [i].SocketIP = newIP
-    # else:
-    #     [sensor_index].SocketIP = newIP
+                # print(input_str) # Debugging
 
-    #We only need sensor settings
-    SensorSettingsWithNames = config['EgoVehicleInfo']['SensorSettingsWithNames']
-    
-    # When no index is mentioned in the input
-    if(sensor_index == ""):
-        SensorSettingsWithNames.replace("SocketIp:0.0.0.0", fullIp)
-    
-    
-    # if config_object['EgoVehicleInfo']['SensorSettingsWithNames']:
-    #     string = config_object.get('EgoVehicleInfo', 'SensorSettingsWithNames').strip().split('\n')
-    #     print(string.find("SocketIp"))
-        # list_in_list = ast.literal_eval(config_object['EgoVehicleInfo']['SensorSettingsWithNames'])
-        # print(list_in_list)
+                # Write new SensorSettingsWithNames to config file
+                config['EgoVehicleInfo']['SensorSettingsWithNames'] = new_settings
+                with open(filename, 'w') as f:
+                    config.write(f)
+                
+                new_match = re.search(pattern, new_settings)
+                socket_ip = new_match.group(2)
+                print(f"Changed Socket IP for sensor {sensor_index}: {socket_ip}")
 
-    # for section in config_object.sections():
-    #     print(f"[{section}]")
-    #     if section == "EgoVehicleInfo":
-    #         for key, value in config_object.items(section):
-    #             if key == "sensorsettingswithnames":
-    #                 print(f"\tfor key {key} -> {value} (value)")
-    #                 if value == "[0]":
-    #                     print ("found it")
+            else:
+                print(f"Sensor data not found for index {sensor_index}")
     
-#Update the password
-# userinfo["password"] = "newpassword"
+    elif len(sys.argv) == 4:
+        filename = sys.argv[1]
+        sensor_index = int(sys.argv[2])
+        newIP = str(sys.argv[3])
+        
+        #Read the configutation file
+        config.read(filename)
+        
+        if config['EgoVehicleInfo']['SensorSettingsWithNames']:
+            input_str=config['EgoVehicleInfo']['SensorSettingsWithNames']
+            input_str = re.sub(r'\s+', '', input_str)
+        
+            pattern = r"\[%d\]={([^}]*).*?SocketIp=([\d\.]+)" % sensor_index
+            match = re.search(pattern, input_str)
 
-# #Write changes back to file
-# with open('config.ini', 'w') as conf:
-#     config_object.write(conf)
+            # If there is a match, extract the SocketIp value and replace it
+            if match:
+                socket_ip = match.group(2)
+                print(f"Socket IP for sensor {sensor_index}: {socket_ip}")
+                new_settings = input_str.replace(socket_ip, newIP)
+                # Adding spaces in the string to look good
+                input_str = re.sub(r'\s+', '', input_str)
+                input_str = re.sub(r'([^\{\}\[\]\(\)=,]+)(=)', r'\1 = ', input_str)
+
+                # print(input_str) # Debugging
+
+                # Write new SensorSettingsWithNames to config file
+                config['EgoVehicleInfo']['SensorSettingsWithNames'] = new_settings
+                with open(filename, 'w') as f:
+                    config.write(f)
+                
+                new_match = re.search(pattern, new_settings)
+                socket_ip = new_match.group(2)
+                print(f"Changed Socket IP for sensor {sensor_index}: {socket_ip}")
+
+            else:
+                print(f"Sensor data not found for index {sensor_index}")
