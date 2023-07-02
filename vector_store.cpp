06d9a0158d4a001443345d6a2ebc33cca20af488
Jawad <syedjawadakhtar@gmail.com>
2023-07-02 14:45:04 +0300

    Specifying size and initializing values

--- vector_store.cpp
+++ vector_store.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	// Vector initialization by pushing values
 	int n = 10;
 	std :: vector< int > v;
 	for (int i=0; i<n; i++){
@@ -15,6 +16,16 @@ int main(int argc, char const *argv[])
 	for (int i=0; i<n; i++){
 		cout << v[i] << " ";
 	} 
+
+	// Specifying size and intializing all values
+
+	int n = 3;
+
+	std :: vector <int> v (n, 10);
+
+	for (int x : v){
+		cout << x << " "; 
+	}	// Prints 3 10s
          
 
 	return 0;
